2117adaa486c5329888f0489edc387da75b6aaa5
Pantea Karimi Babaahmadi <pkarimib@tardy.csail.mit.edu>
2022-04-05 17:08:18 -0400

    more scripts, remove CUDA_VISIBLE_DEVICES

---
+++ paper_scripts/README.md
@@ -0,0 +1,4 @@
+To run an experiment, run:
+```
+CUDA_VISIBLE_DEVICES=0 bash script.sh speaker_name
+```
--- paper_scripts/resolution1024_with_hr_skip_connections.sh
+++ paper_scripts/resolution1024_with_hr_skip_connections.sh
@@ -4,4 +4,4 @@ cd $CODE_DIR
 export PYTHONPATH=$PYTHONPATH:$PWD
 cd  first_order_model
 
-CUDA_VISIBLE_DEVICES=1 python3 run.py --config config/paper_configs/resolution1024_with_hr_skip_connections.yaml --log_dir /video-conf/scratch/pantea_experiments_tardy/resolution1024_with_hr_skip_connections --experiment_name ${person}_resolution1024_with_hr_skip_connections --person_id ${person} --checkpoint /video-conf/scratch/vibhaa_mm_log_directory/vox-cpk.pth.tar
+python3 run.py --config config/paper_configs/resolution1024_with_hr_skip_connections.yaml --log_dir /video-conf/scratch/pantea_experiments_tardy/resolution1024_with_hr_skip_connections --experiment_name ${person}_resolution1024_with_hr_skip_connections --person_id ${person} --checkpoint /video-conf/scratch/vibhaa_mm_log_directory/vox-cpk.pth.tar
