0ba8104e89c6dfb7d0778e1664ee6241265f5957
Pantea Karimi Babaahmadi <pkarimib@tardy.csail.mit.edu>
2022-04-05 13:47:50 -0400

    added paper scripts

---
+++ paper_scripts/generic.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 python3 run.py --config config/overview_exps_for_512_resolution.yaml --log_dir /video-conf/scratch/pantea_experiments_tardy --experiment_name generic_512_kp_at_256_with_hr_skip_connections --person_id generic
