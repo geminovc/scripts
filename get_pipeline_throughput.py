805f8ff7e4d7b9b789b9bd2ce0cd56915b4e4fac
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-04-11 23:27:13 +0000

    av pred time

--- get_pipeline_throughput.py
+++ get_pipeline_throughput.py
@@ -1,5 +1,6 @@
 """ get throughput aggregated over windows of the experiment
 """
+import numpy as np
 def get_throughput(filepath, window):
     windowed_throughput = []
     last_window_start = -1
@@ -8,6 +9,7 @@ def get_throughput(filepath, window):
     old_end_of_prediction_time = 0
     first_prediction_time = 0
     last_prediction_time = 0
+    prediction_times = []
     with open(filepath) as fp:
         line = fp.readline()
         while line:
@@ -16,6 +18,7 @@ def get_throughput(filepath, window):
                 total_predicted_frames += 1
                 x_split = x.split()
                 end_of_prediction_time = float(x_split[10])
+                prediction_times.append(float(x_split[7]))
                 if first_prediction_time == 0:
                     first_prediction_time = end_of_prediction_time
                 print("time diff", (end_of_prediction_time - old_end_of_prediction_time) * 1000)
@@ -33,8 +36,10 @@ def get_throughput(filepath, window):
     last_prediction_time = end_of_prediction_time
     if current_window_len > 0:
         windowed_throughput.append(current_window_len)
-    print("windowed_throughput", [i/window for i in windowed_throughput])
+    #print("windowed_throughput", [i/window for i in windowed_throughput])
+    print("average windowed_throughput", np.average([i/window for i in windowed_throughput]))
     print("total_predicted_frames", total_predicted_frames)
+    print("average prediction time", np.average(prediction_times))
     print("total_throughput", total_predicted_frames / (last_prediction_time - first_prediction_time))
 
 def get_throughput_over_windows(save_dir, window):
@@ -63,6 +68,6 @@ def get_throughput_over_windows(save_dir, window):
     print("windowed_throughput", windowed_throughput)
     return  windowed_throughput
 
-window = 0.03
-get_throughput("/data4/pantea/aiortc/examples/videostream-cli/xiran_1024_with_skip/receiver_output", window)
-#get_throughput("/data4/pantea/nets_scripts/original_resolution512_with_hr_skip_connections/kayleigh/512_kayleigh_10_second_0_1.mp4/run0/receiver.log", window)    
+window = 0.5
+#get_throughput("/data4/pantea/aiortc/examples/videostream-cli/xiran_1024_with_skip/receiver_output", window)
+get_throughput("/data4/pantea/nets_scripts/xiran_1024_resolution1024_without_hr_skip_connections/xiran/0_1.mp4/run0/receiver.log", window)    
