282708ca3513ebceb3c6fe9d7aedbf97e7d02382
pkarimib <pkarimib@mit.edu>
2022-05-24 14:40:58 -0400

    Update get_average_frame.sh

--- dataset_scripts/get_average_frame.sh
+++ dataset_scripts/get_average_frame.sh
@@ -1,19 +1,16 @@
 # generates the average frame to drop a crop box on for each of the original
 # videos for the specified speakers
-# from the sppech to gesture dataset
 
-DIR="/Users/panteababaahmadi/Documents/GitHub/nets_meta/youtube_dataset/dataset"
-for speaker in "tucker" #"jen_psaki" # "xiran" #"fancy_fueko" "seth_meyer" "tucker" #"kayleigh" "trever_noah"
+DIR=$1
+speaker=$2
+for phase in train test
 do
-	for phase in train test
-	do
-	    mkdir -p ${DIR}/${speaker}/averages/${phase}
-	    for file in ${DIR}/${speaker}/original_youtube/${phase}/*
-	    do
-	        filename=$(basename "$file")
-	        fbname=${filename%%.*}
-	        echo $fbname
-	        python3 get_average.py $file ${DIR}/${speaker}/averages/${phase}/${fbname}.jpg
-	    done
-	done
+    mkdir -p ${DIR}/${speaker}/averages/${phase}
+    for file in ${DIR}/${speaker}/original_youtube/${phase}/*
+    do
+	filename=$(basename "$file")
+	fbname=${filename%%.*}
+	echo $fbname
+	python3 get_average.py $file ${DIR}/${speaker}/averages/${phase}/${fbname}.jpg
+    done
 done
