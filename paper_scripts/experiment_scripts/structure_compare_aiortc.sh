d60236943ef63f5615afef9d2affb3cb0c90de33
pkarimib <pkarimib@mit.edu>
2022-05-25 15:34:28 -0400

    redundant

--- paper_scripts/experiment_scripts/structure_compare_aiortc.sh
+++
@@ -1,7 +0,0 @@
-cd ../../end2end_experiments
-# xiran
-python structure_comparison_experiments.py --duration 300 \
---csv-name ./xiran_1024_diget.csv --save-prefix ./xiran_1024 \
---executable-dir /data4/pantea/aiortc/examples/videostream-cli \
---root-dir /video-conf/scratch/pantea/fom_personalized_1024/ \
---person-list xiran --num-runs 1 --window 1 --resolution 1024
