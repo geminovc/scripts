e79b318f0f15c9ea4f8ffcea8a1ccb84bf925fe3
pkarimib <pkarimib@mit.edu>
2022-05-25 11:29:06 -0400

    Update structure_comparison.sh

--- paper_scripts/experiment_scripts/structure_comparison.sh
+++ paper_scripts/experiment_scripts/structure_comparison.sh
@@ -1,10 +1,18 @@
+# Compare different model structures for the prediction pipeline
 EXCE_DIR=$1
 DURATION=$2
 PERSON=$3
 RESOLUTION=$4
-cd ../..
+cd ../../end2end_experiments/
 
-python structure_comparison_experiments.py --duration $DURATION --csv-name ./${PERSON}_${RESOLUTION}_diget.csv --save-prefix ./${PERSON}_${RESOLUTION} --executable-dir "${EXCE_DIR}"  --root-dir /video-conf/scratch/pantea/fom_personalized_${RESOLUTION} --person-list ${PERSON} --num-runs 1 --window $DURATION --resolution ${RESOLUTION}
+python structure_comparison_experiments.py \
+--duration $DURATION --csv-name ./${PERSON}_${RESOLUTION}_diget.csv \
+--save-prefix ./${PERSON}_${RESOLUTION} --executable-dir "${EXCE_DIR}"  \
+--root-dir /video-conf/scratch/pantea/fom_personalized_${RESOLUTION} \
+--person-list ${PERSON} --num-runs 1 --window $DURATION --resolution ${RESOLUTION}
 
 # Example
-# CUDA_VISIBLE_DEVICES=0 python structure_comparison_experiments.py --duration 360 --csv-name ./xiran_1024_diget.csv --save-prefix ./xiran_1024 --executable-dir /data4/pantea/aiortc/examples/videostream-cli --root-dir ./videos/1024/ --person-list xiran --num-runs 1 --window 0.05 --resolution 1024
+# CUDA_VISIBLE_DEVICES=0 python structure_comparison_experiments.py \
+# --duration 360 --csv-name ./xiran_1024_diget.csv --save-prefix ./xiran_1024 \
+# --executable-dir /data4/pantea/aiortc/examples/videostream-cli \
+# --root-dir ./videos/1024/ --person-list xiran --num-runs 1 --window 0.05 --resolution 1024
