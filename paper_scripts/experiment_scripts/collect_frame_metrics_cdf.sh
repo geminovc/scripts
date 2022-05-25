783c7bb5423375d90a0792377fe50e36a005f31b
pkarimib <pkarimib@mit.edu>
2022-05-25 11:22:34 -0400

    Update collect_frame_metrics_cdf.sh

--- paper_scripts/experiment_scripts/collect_frame_metrics_cdf.sh
+++ paper_scripts/experiment_scripts/collect_frame_metrics_cdf.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-python collect_per_frame_stats.py --save-prefix logs --person kayleigh --setting personalized --reference-frame-frequency-list 10 60 --vpx-quantizer-list 2 32 63 --video-name 0_2.mp4 --csv-name data/kayleigh_video0_2_per_frame_data
+cd ../../
 
+python collect_per_frame_stats.py --save-prefix logs \
+--person kayleigh --setting personalized \
+--reference-frame-frequency-list 10 60 \
+--vpx-quantizer-list 2 32 63 \
+--video-name 0_2.mp4 \
+--csv-name data/kayleigh_video0_2_per_frame_data
+
+cd plot_scripts
 ./frame_quality_cdf.R data/kayleigh_video0_2_per_frame_data pdfs/kayleigh_metrics_cdf.pdf
