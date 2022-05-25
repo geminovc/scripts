64f84715b77fb24757949c70b291632e8c9b718a
pkarimib <pkarimib@mit.edu>
2022-05-24 22:49:45 -0400

    Update reencode_at_30fps.sh

--- dataset_scripts/reencode_at_30fps.sh
+++ dataset_scripts/reencode_at_30fps.sh
@@ -1,19 +1,19 @@
-DIR_PATH=/Users/panteababaahmadi/Documents/GitHub/nets_meta/youtube_dataset
+DIR_PATH=$1
 for person in "fancy_fueko" "needle_drop" "xiran" "fancy_fueko"  "jen_psaki"  "kayleigh"  "seth_meyer"  "trever_noah"  "tucker"  "xiran_close_up";
 do
-    for folder in "test"; #"train";
+    for folder in "test" "train";
     do
-        # # 1024
-        # for file in ${DIR_PATH}/dataset_1024/fom_personalized_1024/${person}/${folder}/*;
-        # do
-        #     echo ${file};
-        #     filename=$(basename "$file")
-        # 	fbname=${filename%%.*}
-        #     ffmpeg -y -i $file -filter:v fps=30 ${DIR_PATH}/dataset_1024/fom_personalized_1024_30fps/${person}/${folder}/${fbname}.mp4
+        # 1024
+        for file in ${DIR_PATH}/dataset_1024/fom_personalized_1024/${person}/${folder}/*;
+        do
+            echo ${file};
+            filename=$(basename "$file")
+        	fbname=${filename%%.*}
+            ffmpeg -y -i $file -filter:v fps=30 ${DIR_PATH}/dataset_1024/fom_personalized_1024_30fps/${person}/${folder}/${fbname}.mp4
 
-        # done;
+        done;
 
-        #512
+        # 512
         for file in ${DIR_PATH}/dataset/fom_personalized_512/${person}/${folder}/*;
         do
             echo ${file};
