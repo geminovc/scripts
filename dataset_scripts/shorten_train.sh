782b615c348e85a24ded9dc9e4985349f9c52861
pkarimib <pkarimib@mit.edu>
2022-05-24 22:21:29 -0400

    Update shorten_train.sh

--- dataset_scripts/shorten_train.sh
+++ dataset_scripts/shorten_train.sh
@@ -1,7 +1,7 @@
 # Divide the train videos into clips of 10 second
 DIR=$1
 speaker=$2
-mkdir -p ${DIR}/${speaker}/${speaker}/train
+mkdir -p ${DIR}/${speaker}/${speaker}_dataset/train
 for file in ${DIR}/${speaker}/spatially_cropped/train/*
 do
     val=$(ffprobe -v error -select_streams v:0 -count_frames -show_entries stream=nb_read_frames -print_format csv ${file} | cut -d',' -f2);
@@ -14,7 +14,7 @@ do
         for start in {0..120..10}
         do
             ((end=start+10))
-            ffmpeg -ss $start -to $end  -i $file -c copy ${DIR}/${speaker}/${speaker}/train/id${speaker}_${end}_${fbname}.mp4
+            ffmpeg -ss $start -to $end  -i $file -c copy ${DIR}/${speaker}/${speaker}_dataset/train/id${speaker}_${end}_${fbname}.mp4
         done
     else
         cp $file ${DIR}/${speaker}/${speaker}/train/id${speaker}_10_${fbname}.mp4
