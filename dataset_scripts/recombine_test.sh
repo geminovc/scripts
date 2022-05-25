dfe0b63d3f609cd3619772f89476870c1ea2a872
pkarimib <pkarimib@mit.edu>
2022-05-24 22:24:28 -0400

    Update recombine_test.sh

--- dataset_scripts/recombine_test.sh
+++ dataset_scripts/recombine_test.sh
@@ -1,15 +1,13 @@
-# Divide the videos into clips of 10 second
-DIR="/Users/panteababaahmadi/Documents/GitHub/nets_meta/youtube_dataset/dataset"
-for speaker in "tucker" #"jen_psaki" # "xiran" # "fancy_fueko" "seth_meyer" "tucker" #"kayleigh" "trever_noah" "tucker"
+# Combine sessions (clips) from the same url into a single video 
+DIR=$1
+speaker=$2
+mkdir -p ${DIR}/${speaker}/${speaker}/test
+for start in {1..5..1}
 do
-    mkdir -p ${DIR}/${speaker}/${speaker}/test
-    for start in {1..5..1}
+    echo ${start}
+    for f in ${DIR}/${speaker}/spatially_cropped/test/*_${start}.mp4
     do
-        echo ${start}
-        for f in ${DIR}/${speaker}/spatially_cropped/test/*_${start}.mp4
-        do
-            echo file \'$f\' >> ${DIR}/${speaker}/test_${start}.txt
-        done
-        ffmpeg -safe 0 -f concat -i ${DIR}/${speaker}/test_${start}.txt -c copy ${DIR}/${speaker}/${speaker}/test/id${speaker}_${start}.mp4
+        echo file \'$f\' >> ${DIR}/${speaker}/test_${start}.txt
     done
+    ffmpeg -safe 0 -f concat -i ${DIR}/${speaker}/test_${start}.txt -c copy ${DIR}/${speaker}/${speaker}/test/id${speaker}_${start}.mp4
 done
