456397ad1ecf8e0d0ca8729b2109d2109bf22c9f
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-08-29 21:13:59 +0000

    added resolution to sr bash

--- paper_scripts/experiment_scripts/swinir_lte_baseline.sh
+++ paper_scripts/experiment_scripts/swinir_lte_baseline.sh
@@ -3,12 +3,13 @@
 export CONFIG_PATH='/data4/pantea/nets_scripts/paper_configs/resolution1024_swinir_lte.yaml'
 cd ../../end2end_experiments
 python lr_video_experiments.py \
++--lr-resolutions 64x64 128x128 256x256 \
 --duration 70 --window 1000 --runs 3 \
 --root-dir /video-conf/scratch/pantea/fom_personalized_1024 \
 --people xiran \
---save-prefix /data3/pantea/xiran_256x256lr_swinir_lte_logs \
+--save-prefix /data3/pantea/xiran_swinir_lte_logs \
 --executable-dir /data4/pantea/aiortc/examples/videostream-cli \
---csv data/xiran_256x256lr_swinir_lte_logs \
+--csv data/xiran_swinir_lte_logs \
 --quantizer-list 32 --lr-quantizer-list 40 45 50 55 63 \
 --reference-update-freq 5000 \
 --video-num-range 0 4 --disable-mahimah \
