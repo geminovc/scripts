fab8cc75d0ff11ba253452ec965c02e8bf3ffed0
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-08-29 21:07:15 +0000

    added resolution to bash

--- paper_scripts/experiment_scripts/bicubic_baseline.sh
+++ paper_scripts/experiment_scripts/bicubic_baseline.sh
@@ -3,15 +3,16 @@
 export CONFIG_PATH='/data4/pantea/nets_scripts/paper_configs/resolution1024_bicubic.yaml'
 cd ../../end2end_experiments
 python lr_video_experiments.py \
---duration 70 --window 1000 --runs 3 \
+--lr-resolutions 64x64 128x128 256x256 \
+--duration 70 --window 1000 --runs 1 \
 --root-dir /video-conf/scratch/pantea/fom_personalized_1024 \
 --people xiran \
---save-prefix /data3/pantea/xiran_256x256lr_bicubic_logs \
+--save-prefix /data3/pantea/xiran_bicubic_logs \
 --executable-dir /data4/pantea/aiortc/examples/videostream-cli \
---csv data/xiran_256x256lr_bicubic_logs \
---quantizer-list 32 --lr-quantizer-list 40 45 50 55 63 \
+--csv data/xiran_bicubic_logs \
+--quantizer-list 32 --lr-quantizer-list 32 40 45 50 55 63 \
 --reference-update-freq 5000 \
---video-num-range 0 4 --disable-mahimah --use_bicubic \
+--video-num-range 0 2 --disable-mahimah --use_bicubic \
 
 # plot data
 #cd ../plot_scripts
