0664fc19ad4263c4841193dcdbdf242d55a774ce
Pantea Karimi <pkarimib@mit.edu>
2022-09-01 15:47:44 -0400

    the scripts for fom and bicubic results produced on tardy

--- paper_scripts/experiment_scripts/bicubic_baseline.sh
+++ paper_scripts/experiment_scripts/bicubic_baseline.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # compares Bicubic at a series of low resolutions and different quantizer and bitrates values
-export CONFIG_PATH='/data4/pantea/nets_scripts/paper_configs/resolution1024_bicubic.yaml'
+export CONFIG_PATH='/data1/pantea/nets_scripts/paper_configs/resolution1024_bicubic.yaml'
 cd ../../end2end_experiments
 python lr_video_experiments.py \
---lr-resolutions 64x64 128x128 256x256 \
---duration 70 --window 1000 --runs 1 \
+--lr-resolutions 64x64 128x128 256x256 512x512 \
+--duration 400 --window 1000 --runs 1 \
 --root-dir /video-conf/scratch/pantea/fom_personalized_1024 \
---people xiran \
---save-prefix /data3/pantea/xiran_bicubic_logs \
---executable-dir /data4/pantea/aiortc/examples/videostream-cli \
---csv data/xiran_bicubic_logs \
---quantizer-list 32 --lr-quantizer-list 32 40 45 50 55 63 \
+--people needle_drop kayleigh xiran fancy_fueko adam_neely \
+--save-prefix /data1/pantea/nsdi_fall_2022/bicubic_fixed_quantizer_logs \
+--executable-dir /data1/pantea/aiortc/examples/videostream-cli \
+--csv data/nsdi_fall_2022_bicubic_fixed_quantizer_all_ppl_3_videos \
+--quantizer-list 32 --lr-quantizer-list 63 55 48 40 32 16 8 2 \
 --reference-update-freq 5000 \
 --video-num-range 0 2 --disable-mahimah --use_bicubic \
 
--- paper_scripts/experiment_scripts/fom_baseline.sh
+++ paper_scripts/experiment_scripts/fom_baseline.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # compares FOM different quantizer and bitrates values
-export CONFIG_PATH='/data4/pantea/nets_scripts/paper_configs/resolution1024_fom_standard.yaml'
+export CONFIG_PATH='/data1/pantea/nets_scripts/paper_configs/resolution1024_fom_standard.yaml'
 cd ../../end2end_experiments
 python structure_comparison_experiments.py \
 --resolution 1024 \
---duration 70 --window 70000 --runs 1 \
+--duration 1000  --window 720000 --runs 1 \
 --root-dir /video-conf/scratch/pantea/fom_personalized_1024 \
---people xiran \
---save-prefix /data3/pantea/keypoint_based_models \
---executable-dir /data4/pantea/aiortc/examples/videostream-cli \
---csv data/xiran_keypoint_based_models \
+--people xiran needle_drop kayleigh fancy_fueko adam_neely \
+--save-prefix /data1/pantea/nsdi_fall_2022/keypoint_based_models_logs \
+--executable-dir /data1/pantea/aiortc/examples/videostream-cli \
+--csv data/nsdi_fall_2022_keypoint_based_models \
 --setting-list fom_standard \
 --quantizer-list 32 \
---reference-update-freq 5000 \
---video-num-range 0 0 --disable-mahimah \
+--reference-update-freq-list 30 100 1000  500000 \
+--video-num-range 0 2 --disable-mahimah  \
 
 # plot data
 #cd ../plot_scripts
