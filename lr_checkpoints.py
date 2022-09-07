223146d188dd2c67c8dc3d8f9aebb14fa073be8b
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-09-07 15:15:17 +0000

    added more checkpoints

--- lr_checkpoints.py
+++ lr_checkpoints.py
@@ -7,10 +7,21 @@ checkpoint_dict = {
                                  },
 
     'resolution1024_occlusion_aware':{
-        'needle_drop':{},
+        'needle_drop':{
+                '64x64': { 'lrquantizer-1':{}},
+
+                '128x128': { 'lrquantizer-1':{}},
+
+                '256x256': { 'lrquantizer-1':{}},
+
+                '512x512': {'lrquantizer-1':{}}
+                      },
 
         'xiran': {
-                '64x64': {},
+                '64x64': { 'lrquantizer-1': { 
+                                            45: '/video-conf/scratch/vibhaa_tardy/encoder_fixed_bitrate/xiran1024_lr64_tgt45Kb 06_09_22_01.59.42/00000029-checkpoint.pth.tar'
+                                            }
+                         },
 
                 '128x128': {
                             'lrquantizer-1':{
@@ -30,10 +41,57 @@ checkpoint_dict = {
                 '512x512':{}
                  },
 
-        'kayleigh': {},
+        'kayleigh': { 
+                '64x64': { 'lrquantizer-1':{
+                                            45: '/video-conf/scratch/vibhaa_tardy/encoder_fixed_bitrate/kayleigh1024_lr64_tgt45Kb 06_09_22_22.13.09/00000029-checkpoint.pth.tar'
+                                            }
+                        },
 
-        'adam_neely': {},
+                '128x128': { 'lrquantizer-1':{
+                                            15: '/video-conf/scratch/vibhaa_lam2/encoder_fixed_bitrate/kayleigh1024_128lr_tgt15Kb 07_09_22_05.02.52/00000029-checkpoint.pth.tar',
+                                            45: '/video-conf/scratch/vibhaa_tardy/encoder_fixed_bitrate/kayleigh1024_lr128_tgt45Kb 06_09_22_22.12.44/00000029-checkpoint.pth.tar'
+                                             }
+                           },
 
-        'fancy_fueko': {}
+                '256x256': { 'lrquantizer-1':{
+                                            75: '/video-conf/scratch/vibhaa_tardy/encoder_fixed_bitrate/kayleigh1024_lr256_tgt75Kb 06_09_22_22.15.44/00000029-checkpoint.pth.tar',
+                                            105: '/video-conf/scratch/vibhaa_lam2/encoder_fixed_bitrate/kayleigh1024_256lr_tgt105Kb 07_09_22_05.04.40/00000029-checkpoint.pth.tar'
+                                             }
+                           },
+
+                '512x512': {'lrquantizer-1':{}}
+                    },
+
+        'adam_neely': {
+                '64x64': {'lrquantizer-1':{
+                                            45: '/video-conf/scratch/vibhaa_lam1/encoder_fixed_bitrate/adam_neely1024_64lr_tgt45Kb 07_09_22_13.21.45/00000029-checkpoint.pth.tar'
+                                          }
+                         },
+
+                '128x128': {'lrquantizer-1':{
+                                            15: '/video-conf/scratch/vibhaa_lam1/encoder_fixed_bitrate/adam_neely1024_128lr_tgt15Kb 07_09_22_13.20.24/00000029-checkpoint.pth.tar',
+                                            45: '/video-conf/scratch/vibhaa_lam1/encoder_fixed_bitrate/adam_neely1024_128lr_tgt45Kb 07_09_22_13.21.19/00000029-checkpoint.pth.tar'
+                                            }
+                           },
+
+                '256x256': {'lrquantizer-1':{
+                                            45: '/video-conf/scratch/vibhaa_lam1/encoder_fixed_bitrate/adam_neely1024_256lr_tgt45Kb 07_09_22_13.19.39/00000029-checkpoint.pth.tar',
+                                            75: '/video-conf/scratch/vibhaa_lam1/encoder_fixed_bitrate/adam_neely1024_256lr_tgt75Kb 07_09_22_13.19.06/00000029-checkpoint.pth.tar',
+                                            105: '/video-conf/scratch/vibhaa_lam1/encoder_fixed_bitrate/adam_neely1024_256lr_tgt105Kb 07_09_22_13.18.03/00000029-checkpoint.pth.tar'
+                                            }
+                           },
+
+                '512x512': {'lrquantizer-1':{}}
+                      },
+
+        'fancy_fueko': {
+                '64x64': { 'lrquantizer-1':{}},
+
+                '128x128': { 'lrquantizer-1':{}},
+
+                '256x256': { 'lrquantizer-1':{}},
+
+                '512x512': {'lrquantizer-1':{}}
+                        }
     }
 }
