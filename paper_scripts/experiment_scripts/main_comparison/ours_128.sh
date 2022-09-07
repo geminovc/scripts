7d1f92fba0bdce0ec6591fefe6234bb115c667d0
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-09-07 00:59:40 +0000

    added lr128 for main methods

--- paper_scripts/experiment_scripts/main_comparison/ours_128.sh
+++ paper_scripts/experiment_scripts/main_comparison/ours_128.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-# main experiment for Ours 256x256
+# main experiment for Ours 128x128
 cd ../../../end2end_experiments
 
 # full-range quantizer
 python lr_video_experiments.py \
---lr-resolutions 256x256 \
+--lr-resolutions 128x128 \
 --duration 2400 --window 1000 --runs 1 \
 --root-dir /video-conf/scratch/pantea/fom_personalized_1024 \
 --people xiran \
 --save-prefix /data4/pantea/nsdi_fall_2022/main_comparison/ours_full_quantizer \
 --executable-dir /data4/pantea/aiortc/examples/videostream-cli \
---csv /data4/pantea/nsdi_fall_2022/main_comparison/data/ours_lr256_full_quantizer \
+--csv /data4/pantea/nsdi_fall_2022/main_comparison/data/ours_lr128_full_quantizer \
 --quantizer-list 32 --lr-quantizer-list -1 \
---lr-target-bitrate-list 45 75 105 \
+--lr-target-bitrate-list 15 \
 --configs-dir /data4/pantea/nets_scripts/paper_configs/exps_overview \
 --generator-type occlusion_aware --resolution 1024 \
 --video-num-range 0 4 --disable-mahimah \
