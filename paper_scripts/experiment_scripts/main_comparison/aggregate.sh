1e63ecc0c9a2a76ca04d812f7e73858e892a2726
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-09-07 02:13:52 +0000

    move storing place for mem issues

--- paper_scripts/experiment_scripts/main_comparison/aggregate.sh
+++ paper_scripts/experiment_scripts/main_comparison/aggregate.sh
@@ -5,7 +5,7 @@ cd ../../end2end_experiment
 
 
 python aggregate_approach_comparison_data.py \
---data-paths /data4/pantea/nsdi_fall_2022/main_comparison/data/ours_lr256_full_quantizer /data4/pantea/nsdi_fall_2022/main_comparison/data/ours_lr128_full_quantizer \
+--data-paths /data3/pantea/nsdi_fall_2022/main_comparison/data/ours_lr256_full_quantizer /data4/pantea/nsdi_fall_2022/main_comparison/data/ours_lr128_full_quantizer \
 --settings 256x256 128x128 \
 --csv-name /data4/pantea/nsdi_fall_2022/lr_resolution_comparison/data/ours_full_quantizer \
 --columns-names setting kbps psnr ssim ssim_db orig_lpips lr_resolution lr_quantizer lr_target_bitrate \
--- paper_scripts/experiment_scripts/main_comparison/ours.sh
+++ paper_scripts/experiment_scripts/main_comparison/ours.sh
@@ -9,9 +9,9 @@ python lr_video_experiments.py \
 --duration 2400 --window 1000 --runs 1 \
 --root-dir /video-conf/scratch/pantea/fom_personalized_1024 \
 --people xiran \
---save-prefix /data4/pantea/nsdi_fall_2022/main_comparison/ours_full_quantizer \
+--save-prefix /data3/pantea/nsdi_fall_2022/main_comparison/ours_full_quantizer \
 --executable-dir /data4/pantea/aiortc/examples/videostream-cli \
---csv /data4/pantea/nsdi_fall_2022/main_comparison/data/ours_lr256_full_quantizer \
+--csv /data3/pantea/nsdi_fall_2022/main_comparison/data/ours_lr256_full_quantizer \
 --quantizer-list 32 --lr-quantizer-list -1 \
 --lr-target-bitrate-list 45 75 105 \
 --configs-dir /data4/pantea/nets_scripts/paper_configs/exps_overview \
