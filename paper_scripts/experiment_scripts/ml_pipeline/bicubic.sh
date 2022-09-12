e4861d49c383177d10b90b8e8e00cbc08260a30e
Pantea Karimi <pkarimib@mit.edu>
2022-09-12 14:02:50 -0400

    added new config

--- paper_scripts/experiment_scripts/ml_pipeline/bicubic.sh
+++ paper_scripts/experiment_scripts/ml_pipeline/bicubic.sh
@@ -6,7 +6,7 @@ FINAL_DIR=/video-conf/scratch/pantea_tardy/final_results/bicubic
 MAIN_CONFIG_DIR=/data1/pantea/aiortc/nets_implementation/first_order_model/config/paper_configs/exps_overview/bicubic
 for person in "adam_neely" "xiran" "fancy_fueko" "needle_drop" "kayleigh";
 do
-    for setting in lr128_tgt15Kb lr256_tgt45Kb lr256_tgt75Kb lr512_tgt180Kb lr512_tgt420Kb
+    for setting in lr128_tgt15Kb lr256_tgt45Kb lr256_tgt75Kb lr256_tgt105Kb lr512_tgt180Kb lr512_tgt420Kb
     do
         CUDA_VISIBLE_DEVICES=0 python run.py --config ${MAIN_CONFIG_DIR}/${setting}.yaml \
 		--experiment_name single_source --person_id ${person} --mode reconstruction \
