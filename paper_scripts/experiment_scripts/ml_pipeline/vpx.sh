6ef9ff3f09fdd4008fe620af9c92afc63b994c12
Pantea Karimi <pkarimib@mit.edu>
2022-09-11 23:15:40 -0400

    ml scripts

---
+++ paper_scripts/experiment_scripts/ml_pipeline/vpx.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# main ML experiment for Bicubic 
+cd /data1/pantea/aiortc/nets_implementation/first_order_model
+FINAL_DIR=/video-conf/scratch/pantea_tardy/final_results/vpx
+MAIN_CONFIG_DIR=/data1/pantea/aiortc/nets_implementation/first_order_model/config/paper_configs/exps_overview/vpx
+for person in "adam_neely" "xiran" "fancy_fueko" "needle_drop" "kayleigh";
+do
+    for setting in lr128_tgt15Kb lr256_tgt45Kb lr256_tgt75Kb lr512_tgt180Kb lr512_tgt420Kb
+    do
+        CUDA_VISIBLE_DEVICES=0 python run.py --config ${MAIN_CONFIG_DIR}/${setting}.yaml \
+		--experiment_name single_source --person_id ${person} --mode reconstruction \
+		--checkpoint ${FINAL_DIR}/${setting}/${person}/00000029-checkpoint.pth.tar
+    done
+done
