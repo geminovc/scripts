90159079c9f7c87c12d7e316c8454a20fa54e29f
pkarimib <pkarimib@mit.edu>
2022-09-13 10:06:29 -0400

    Update bicubic.sh

--- paper_scripts/experiment_scripts/ml_pipeline/bicubic.sh
+++ paper_scripts/experiment_scripts/ml_pipeline/bicubic.sh
@@ -8,7 +8,7 @@ for person in "adam_neely" "xiran" "fancy_fueko" "needle_drop" "kayleigh";
 do
     for setting in lr128_tgt15Kb lr256_tgt45Kb lr256_tgt75Kb lr256_tgt105Kb lr512_tgt180Kb lr512_tgt420Kb
     do
-        CUDA_VISIBLE_DEVICES=0 python run.py --config ${MAIN_CONFIG_DIR}/${setting}.yaml \
+        CUDA_VISIBLE_DEVICES=1 python run.py --config ${MAIN_CONFIG_DIR}/${setting}.yaml \
 		--experiment_name single_source --person_id ${person} --mode reconstruction \
 		--checkpoint ${FINAL_DIR}/${setting}/${person}/00000029-checkpoint.pth.tar
     done
