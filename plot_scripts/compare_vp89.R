44d052fda2689e41053eef2767aa6959e611df69
vibhaa <vibhaa@mit.edu>
2022-09-12 23:43:59 -0400

    vp8/9 comparison

---
+++ plot_scripts/compare_vp89.R
@@ -0,0 +1,50 @@
+#!/usr/bin/env Rscript
+# This script compares quality bpp for all schemes
+
+source("style.R")
+
+args <- commandArgs(trailingOnly=TRUE)
+file <- "../data/bicubic_vp89_bitrate_comparison_data"
+plot_filename <- "pdfs/bicubic_vp89_bitrate_comparison.pdf"
+data<-read.csv(file)
+
+ssim_plot <- ggplot(data, aes(x=kbps,y=ssim_db,color=setting,linetype=setting)) + 
+        geom_line(size=0.8) +
+        geom_point(size=2) +
+        xlim(0, 110) +  
+
+        labs(y="SSIM (dB)", x="Kbps") 
+ggsave(plot_filename, width=12.2,height=5)
+  
+psnr_plot <- ggplot(data, aes(x=kbps,y=psnr,color=setting,linetype=setting)) + 
+        geom_line(size=0.8) +
+        geom_point(size=2) +
+        xlim(0, 110) +  
+
+        theme(legend.text=element_text(size=rel(1)), legend.key.size=unit(15,"points"), legend.position="none",
+              legend.box.margin=margin(-10,-10,-10,-10), legend.title=element_blank(),
+              legend.margin=margin(c(0,0,0,0))) + 
+        labs(y="PSNR (dB)", x="Kbps") 
+
+lpips_plot <- ggplot(data, aes(x=kbps,y=lpips,color=setting,linetype=setting)) + 
+        geom_line(size=0.8) +
+        geom_point(size=2) + 
+        xlim(0, 110) +  
+
+        labs(y="LPIPS", x="Kbps") 
+ 
+
+legend <- get_legend(psnr_plot + theme(legend.position="top"))
+
+
+prow <- plot_grid(ssim_plot + theme(legend.position="none"),
+                  psnr_plot + theme(legend.position="none"),
+                  lpips_plot + theme(legend.position="none"),
+                  ncol = 3, align = "v", axis = "l")
+
+# this tells it what order to put it in
+# so basically tells it put legend first then plots with th legend height 20% of the
+# plot
+p <- plot_grid(legend, prow, rel_heights=c(.2,1), ncol =1)
+
+ggsave(plot_filename, width=12.2, height=5)
