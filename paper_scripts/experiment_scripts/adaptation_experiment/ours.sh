6e216233b50b2422cb534fcbcae03d927875c0ec
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-09-17 16:17:50 +0000

    scripts for adaptation

--- paper_scripts/experiment_scripts/adaptation_experiment/ours.sh
+++ paper_scripts/experiment_scripts/adaptation_experiment/ours.sh
@@ -3,18 +3,19 @@
 # main experiment for Ours 256x256
 cd ../../../end2end_experiments
 
-trace=test_with_frame_save
+trace=paper_ours_shrink
 person=needle_drop
-save_prefix=/data4/pantea/ours_${trace}_q250_enable_lr_gcc
-
+save_prefix=/data4/pantea/nsdi_fall_2022/adaptation/ours_everything_comparison_perfect_estimator_shrinked_trace
+window=1000
+log_dir=${save_prefix}/${person}/1.mp4/lrquantizer-1/quantizer32/run0
 
 # full-range quantizer
 python adaptation.py \
 --lr-enable-gcc \
 --uplink-trace /data4/pantea/nets_scripts/traces/${trace} \
 --downlink-trace /data4/pantea/nets_scripts/traces/12mbps_trace \
---duration 500  --window 1000 --runs 1 \
---root-dir /data4/pantea/fom_dataset_9min \
+--duration 3600  --window ${window} --runs 1 \
+--root-dir /data4/pantea/fom_dataset_adaptation \
 --people ${person} \
 --save-prefix ${save_prefix} \
 --executable-dir /data4/pantea/aiortc/examples/videostream-cli \
@@ -22,18 +23,28 @@ python adaptation.py \
 --quantizer-list 32 --lr-quantizer-list -1 \
 --configs-dir /data4/pantea/nets_scripts/paper_configs/exps_overview \
 --generator-type occlusion_aware --resolution 1024 \
---video-num-range 0 0 --disable-mahimahi \
+--video-num-range 0 0 --disable-mahimahi --just-aggregate \
+
 
 cd /data4/pantea/nets_scripts/post_experiment_process
 
 python plot_bw_trace_vs_estimation.py \
-	--log-path ${save_prefix}/${person}/1.mp4/lrquantizer-1/quantizer32/run0/sender.log \
+	--log-path ${log_dir}/sender.log \
 	--trace-path /data4/pantea/nets_scripts/traces/${trace} \
-	--save-dir ${save_prefix}/${person}/1.mp4/lrquantizer-1/quantizer32/run0 \
+	--save-dir ${log_dir} \
 	--output-name sender \
-	--window 1000
+	--window ${window}
+
+
+python get_metrics_timeseries.py \
+	--template-output ${log_dir}/timeseries_sender_w${window}_ms.csv \
+	--save-dir ${log_dir} \
+	--video-path-1 /data4/pantea/fom_dataset_adaptation/needle_drop/test/1.mp4 \
+	--video-path-2 ${log_dir}/received.mp4 \
+	--window ${window}
+
 
 python estimate_rtt_at_sender.py \
-	--log-path ${save_prefix}/${person}/1.mp4/lrquantizer-1/quantizer32/run0/sender.log \
-	--save-dir ${save_prefix}/${person}/1.mp4/lrquantizer-1/quantizer32/run0 \
+	--log-path ${log_dir}/sender.log \
+	--save-dir ${log_dir} \
 	--output-name sender
