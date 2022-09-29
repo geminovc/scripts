deb0450279c1f6f4ab3816bc71037e70a7e68811
vibhaa <vibhaa@mit.edu>
2022-09-29 12:15:02 -0400

    more options for copying

--- post_experiment_process/reorganize_training_folders.py
+++ post_experiment_process/reorganize_training_folders.py
@@ -2,9 +2,10 @@ import glob
 import os
 
 fixed_bitrate_exps = False
-model_comparisons = True
-base_dir = '/video-conf/scratch/vibhaa_lam1/model_comparison'
-target_folder = '/video-conf/scratch/vibhaa_lam2/final_results/model_comparisons'
+model_comparisons = False
+encoder_effect = True
+base_dir = '/video-conf/scratch/vibhaa_lam1/encoder_fixed_bitrate'
+target_folder = '/video-conf/scratch/vibhaa_lam2/final_results/encoder_effect'
 model_types = ['fomm', 'fomm_skip_connections', 'fomm_skip_connections_lr_in_decoder', \
         'fomm_3_pathways_with_occlusion', 'sme_3_pathways_with_occlusion', 'pure_upsampling']
 
@@ -27,3 +28,11 @@ for speaker in ['adam_neely', 'needle_drop', 'xiran', 'fancy_fueko', 'kayleigh']
                         print(f'copying {folder} to {target_folder}/{model_type}/{speaker}')
                         os.system(f'cp -r "{folder}" {target_folder}/{model_type}/{speaker}')
 
+    if encoder_effect:
+        lr_size = 128
+        for bitrate in ['15Kb', '45Kb', '75Kb', '_random']:
+            for folder in glob.glob(f'{base_dir}/{speaker}1024_{lr_size}lr_tgt{bitrate} *'):
+                if os.path.exists(os.path.join(folder, f'00000029-checkpoint.pth.tar')):
+                    if not os.path.exists(f'{target_folder}/{lr_size}_tgt{bitrate}/{speaker}'):
+                        print(f'copying {folder} to {target_folder}')
+                        os.system(f'cp -r "{folder}" {target_folder}/lr{lr_size}_tgt{bitrate[:-2]}/{speaker}')
