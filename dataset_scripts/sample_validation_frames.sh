fbc3d5b04cb92d88cce0137b1d891b21d7705519
vibhaa <vibhaa@mit.edu>
2022-10-26 13:46:56 -0400

    minor change to sampling script

--- dataset_scripts/sample_validation_frames.sh
+++ dataset_scripts/sample_validation_frames.sh
@@ -1,22 +1,24 @@
-#!/bin/sh
-path="/video-conf/scratch/pantea/fom_personalized_1024"
+#!/bin/bash
+path="/video-conf/scratch/pantea/fom_personalized_512"
+path2="/video-conf/scratch/vibhaa_tardy/dataset_256"
 
 for speaker in "adam_neely" "xiran" "fancy_fueko" "needle_drop" "kayleigh";
 do
     x=0
     for i in 1 2 3 4;
     do
-        mkdir -p "${path}/${speaker}/validation/${x}"
-        ffmpeg -ss 00:00:05.01 -i "${path}/${speaker}/test/${i}.mp4" \
-            -frames:v 1 "${path}/${speaker}/validation/${x}/source.png" 
-        ffmpeg -ss 00:01:13.01 -i "${path}/${speaker}/test/${i}.mp4" \
-            -frames:v 1 "${path}/${speaker}/validation/${x}/target.png" 
+        mkdir -p "${path2}/${speaker}/validation/${x}"
+        ffmpeg -y -ss 00:00:05.01 -i "${path}/${speaker}/test/${i}.mp4" \
+            -frames:v 1 "${path2}/${speaker}/validation/${x}/source.png" 
+        ffmpeg -y -ss 00:00:07.01 -i "${path}/${speaker}/test/${i}.mp4" \
+            -frames:v 1 "${path2}/${speaker}/validation/${x}/target.png" 
         ((x=x+1))
         
-        ffmpeg -ss 00:01:35.01 -i "${path}/${speaker}/test/${i}.mp4" \
-            -frames:v 1 "${path}/${speaker}/validation/${x}/source.png" 
-        ffmpeg -ss 00:00:20.01 -i "${path}/${speaker}/test/${i}.mp4" \
-            -frames:v 1 "${path}/${speaker}/validation/${x}/source.png" 
+        mkdir -p "${path2}/${speaker}/validation/${x}"
+        ffmpeg -y -ss 00:00:02.01 -i "${path}/${speaker}/test/${i}.mp4" \
+            -frames:v 1 "${path2}/${speaker}/validation/${x}/source.png" 
+        ffmpeg -y -ss 00:00:08.01 -i "${path}/${speaker}/test/${i}.mp4" \
+            -frames:v 1 "${path2}/${speaker}/validation/${x}/target.png" 
         ((x=x+1))
     done
 done
