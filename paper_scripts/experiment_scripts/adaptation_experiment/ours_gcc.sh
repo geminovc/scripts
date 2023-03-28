049c479286ea714d03a89e304e4eeef5856fc545
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2023-03-28 17:36:06 +0000

    e2e gcc ours script

--- paper_scripts/experiment_scripts/adaptation_experiment/ours_gcc.sh
+++ paper_scripts/experiment_scripts/adaptation_experiment/ours_gcc.sh
@@ -4,9 +4,9 @@
 cd ../../../end2end_experiments
 nets_scripts_dir=/data4/pantea/nets_scripts #change
 aiortc_dir=/data4/pantea/aiortc  #change
-trace=paper_ours_shrink
+trace=paper_ours
 person=needle_drop
-save_prefix=/data4/pantea/adaptation_pipeline/ours_gcc #change
+save_prefix=/data4/pantea/adaptation_end2end/ours_gcc #change
 window=1000
 video_num=1
 video_root=/video-conf/scratch/pantea_mapmaker/fom_dataset_adaptation
@@ -17,7 +17,7 @@ python adaptation.py \
 --lr-enable-gcc \
 --uplink-trace /video-conf/scratch/pantea_mapmaker/traces/${trace} \
 --downlink-trace /video-conf/scratch/pantea_mapmaker/traces/12mbps_trace \
---duration 500  --window ${window} --runs 1 \
+--duration 600 --window ${window} --runs 1 \
 --root-dir ${video_root} \
 --people ${person} \
 --save-prefix ${save_prefix} \
@@ -31,8 +31,12 @@ python adaptation.py \
 
 cd ${nets_scripts_dir}/post_experiment_process
 
+mm-graph ${log_dir}/mahimahi.log 100 --no-port --no-display --plot-direction \
+	egress > ${log_dir}/mahimahi.eps 2> ${log_dir}/mmgraph.log
+
 python plot_bw_trace_vs_estimation.py \
-	--log-path ${log_dir}/sender.log \
+	--log-dir ${log_dir} \
+	--end-point sender \
 	--trace-path /video-conf/scratch/pantea_mapmaker/traces/${trace} \
 	--save-dir ${log_dir} \
 	--output-name sender \
@@ -40,14 +44,8 @@ python plot_bw_trace_vs_estimation.py \
 
 
 python get_metrics_timeseries.py \
-	--template-output ${log_dir}/compression_timeseries_sender_w${window}_ms.csv \
+	--template-output ${log_dir}/timeseries_sender_w${window}_ms.csv \
 	--save-dir ${log_dir} \
 	--video-path-1 ${video_root}/${person}/test/${video_num}.mp4 \
 	--video-path-2 ${log_dir}/received.mp4 \
 	--window ${window}
-
-
-#python estimate_rtt_at_sender.py \
-#	--log-path ${log_dir}/sender.log \
-#	--save-dir ${log_dir} \
-#	--output-name sender
