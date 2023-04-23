4ea26a00da232383ee7b5d28670b5219da77585b
vibhaa <vibhaa@mit.edu>
2023-04-23 19:01:32 -0400

    finishing up compute table + small bug fix

--- paper_scripts/experiment_scripts/compute_table.sh
+++ paper_scripts/experiment_scripts/compute_table.sh
@@ -1,10 +1,18 @@
 ## ACCURACY
 # depthwise full model accuracy
-python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_lam2_new/depthwise_personalization  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/personalization_depthwise --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
+python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_lam2_new/depthwise_personalization  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compute_experiments/personalization_depthwise --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
 
 # regular full model accuracy
-python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_mm_log_directory/personalization/  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/personalization --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
+python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_mm_log_directory/personalization/  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compuute_experiments_personalization --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
 
+# netadapted ones
+for conv_type in 'depthwise' 'normal';
+do 
+    for percent in '10' '1.5';
+    do 
+        python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vedantha/paper_trials/${conv_type}/${percent}  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compute_experiments/${conv_type}_${percent}_percent --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
+    done
+done
 
 ## PARAMETERS AND MACS
 # regular conv
@@ -23,5 +31,15 @@ do
 done
 
 
+## TIMING (needs to be rerun on each platform)
+for conv_type in 'depthwise' 'normal';
+do 
+    # full models
+    CONV_TYPE=${conv_type} python measure_times.py --resolution 512 --config config/paper_configs/personalization/resolution512_our_model.yaml
 
-# timing (needs to be rerun on each platform)
+    # netadapted versions
+    for percent in '10' '1.5';
+    do 
+        CONV_TYPE=${conv_type} python measure_times.py --config config/paper_configs/netadapt/netadapt.yaml --resolution 512 --netadapt_checkpoint /video-conf/scratch/vedantha/paper_trials/${conv_type}/${percent}/generic/00000038-checkpoint.pth.tar
+    done
+done
