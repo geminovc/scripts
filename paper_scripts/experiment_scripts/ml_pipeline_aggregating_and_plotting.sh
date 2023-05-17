e81c892bfcde5220c9fceb6a028aea48f2973d4a
vibhaa <vibhaa@mit.edu>
2023-05-17 14:26:36 -0400

    appendix figures

--- paper_scripts/experiment_scripts/ml_pipeline_aggregating_and_plotting.sh
+++ paper_scripts/experiment_scripts/ml_pipeline_aggregating_and_plotting.sh
@@ -5,6 +5,7 @@ python collect_results_from_ml_pipeline.py --approaches-to-compare design_model_
 python collect_results_from_ml_pipeline.py --approaches-to-compare main_exp:SwinIR main_exp:fomm main_exp:bicubic main_exp:vp9_bicubic main_exp:ours main_exp:vpx main_exp:vp9_ours main_exp:vp9 --base-dir-list /video-conf/scratch/pantea_tardy/final_results/swinir /video-conf/scratch/vibhaa_lam2/final_results/model_comparisons /video-conf/scratch/pantea_tardy/final_results/bicubic  /video-conf/scratch/vibhaa_tardy/vp9/bicubic /video-conf/scratch/vibhaa_tardy/final_results/training_with_encoder/ /video-conf/scratch/pantea_tardy/final_results/vpx /video-conf/scratch/vibhaa_lam2_new/vp9_summary_before_recalibrating /video-conf/scratch/vibhaa_tardy/vp9/vp9  --person-list needle_drop fancy_fueko adam_neely kayleigh xiran  --people-for-strip kayleigh xiran needle_drop fancy_fueko --save-prefix ../data/full_comparison_with_vp9_after_recalibrating --csv-name summary.csv --img-width 1024 --video-num 2 --frame-num 1634  --summarize
 # data was wrangled to remove "ours" for 75Kb and 105Kb and replace with "vp9_ours", and remaining rows of "vp9_ours" were removed and saved in wrangled_summary.csv
 ./main_comparison.R
+./main_comparison_low_bitrate.R
 
 # main exp 3 cdfs for low-bitrate with VP9
 python collect_results_from_ml_pipeline.py --approaches-to-compare main_exp:SwinIR main_exp:fomm main_exp:bicubic main_exp:ours main_exp:vpx main_exp:vp9_ours main_exp:vp9_bicubic main_exp:vp9 --base-dir-list /video-conf/scratch/pantea_tardy/final_results/swinir /video-conf/scratch/vibhaa_lam2/final_results/model_comparisons /video-conf/scratch/pantea_tardy/final_results/bicubic /video-conf/scratch/vibhaa_tardy/final_results/training_with_encoder/ /video-conf/scratch/pantea_tardy/final_results/vpx /video-conf/scratch/vibhaa_lam2_new/vp9_summary_before_recalibrating /video-conf/scratch/vibhaa_tardy/vp9/bicubic /video-conf/scratch/vibhaa_tardy/vp9/vp9  --person-list needle_drop fancy_fueko adam_neely kayleigh xiran  --people-for-strip kayleigh xiran needle_drop fancy_fueko --save-prefix ../data/full_comparison_with_vp9_after_recalibrating --csv-name frame_cdf.csv --img-width 1024 --video-num 2 --frame-num 1634  --cdf
@@ -36,4 +37,10 @@ python collect_results_from_ml_pipeline.py --approaches-to-compare personalizati
 # adaptation experiment
 python aggregate_adaptation_results.py --save-prefix ../data/adaptation --approaches-to-compare vpx ours --base-dir-list /data4/pantea/nsdi_fall_2022/adaptation/final_results/vpx_everything_comparison_perfect_estimator_shrinked_trace/resolution1024x1024/needle_drop/1.mp4/quantizer-1/vpx_min550000_default550000_max550000bitrate/run0/ /data4/pantea/nsdi_fall_2022/adaptation/final_results/ours_everything_comparison_perfect_estimator_shrinked_trace/needle_drop/1.mp4/lrquantizer-1/quantizer32/run0/
 ./adaptation_facet.R
+
+# appendix figures
+for person in "fancy_fueko" "xiran" "needle_drop" "kayleigh"; 
+    do
+    python collect_results_from_ml_pipeline.py --approaches-to-compare main_exp:bicubic main_exp:vp9_bicubic main_exp:ours main_exp:vpx main_exp:vp9_ours main_exp:vp9 --base-dir-list /video-conf/scratch/pantea_tardy/final_results/bicubic /video-conf/scratch/vibhaa_tardy/vp9/bicubic /video-conf/scratch/vibhaa_tardy/final_results/training_with_encoder/ /video-conf/scratch/pantea_tardy/final_results/vpx /video-conf/scratch/vibhaa_lam2_new/vp9_summary_before_recalibrating /video-conf/scratch/vibhaa_tardy/vp9/vp9  --person-list ${person}  --people-for-strip ${person} --save-prefix ../data/full_comparison_with_vp9_after_recalibrating --csv-name dummy.csv --img-width 1024 --video-num 2 --frame-num 637  --summarize
+done
 
