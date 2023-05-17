b412e064a5409e8965c0a90de1c87ce4d2c469b2
vibhaa <vibhaa@mit.edu>
2023-05-17 14:10:09 -0400

    compute table

--- paper_scripts/experiment_scripts/compute_table.sh
+++ paper_scripts/experiment_scripts/compute_table.sh
@@ -1,16 +1,16 @@
 ## ACCURACY
 # depthwise full model accuracy
-python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_lam2_new/depthwise_personalization  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compute_experiments/personalization_depthwise --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
+python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_lam2_new/depthwise_personalization  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compute_experiments/depthwise_full --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
 
 # regular full model accuracy
-python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_mm_log_directory/personalization/  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compuute_experiments_personalization --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
+python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_mm_log_directory/personalization/  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compute_experiments/normal_full --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
 
 # netadapted ones
 for conv_type in 'depthwise' 'normal';
 do 
     for percent in '10' '1.5';
     do 
-        python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vedantha/paper_trials/${conv_type}/${percent}  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compute_experiments/${conv_type}_${percent}_percent --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
+        python collect_results_from_ml_pipeline.py --approaches-to-compare personalization --base-dir-list /video-conf/scratch/vibhaa_lam1_new/netadapt/aggregated_results_only_generator_adapted/only_gen/512/${conv_type}_${percent}  --person-list kayleigh jen_psaki trevor_noah seth_meyers needle_drop --people-for-strip needle_drop seth_meyers jen_psaki --save-prefix ../data/compute_experiments/only_gen_512_netadapt/${conv_type}_${percent}_percent --csv-name summary.csv --img-width 512 --video-num 2 --frame-num 1952 --summarize
     done
 done
 
@@ -26,7 +26,7 @@ for conv_type in 'depthwise' 'normal';
 do 
     for percent in '10' '1.5';
     do 
-        CONV_TYPE=${conv_type} CUDA_VISIBLE_DEVICES=3 python run.py --config config/paper_configs/netadapt/netadapt.yaml --experiment_name single_source_profile --person_id needle_drop --mode reconstruction --netadapt_checkpoint /video-conf/scratch/vedantha/paper_trials/${conv_type}/${percent}/generic/00000038-checkpoint.pth.tar --profile;
+        CONV_TYPE=${conv_type} CUDA_VISIBLE_DEVICES=3 python run.py --config config/paper_configs/netadapt/netadapt.yaml --experiment_name single_source_profile --person_id needle_drop --mode reconstruction --netadapt_checkpoint /video-conf/scratch/vibhaa_lam1_new/netadapt/aggregated_results_only_generator_adapted/only_gen/512/${conv_type}_${percent}/generic/00000038-checkpoint.pth.tar --profile;
     done
 done
 
@@ -40,6 +40,10 @@ do
     # netadapted versions
     for percent in '10' '1.5';
     do 
-        CONV_TYPE=${conv_type} python measure_times.py --config config/paper_configs/netadapt/netadapt.yaml --resolution 512 --netadapt_checkpoint /video-conf/scratch/vedantha/paper_trials/${conv_type}/${percent}/generic/00000038-checkpoint.pth.tar
+        CONV_TYPE=${conv_type} python measure_times.py --config config/paper_configs/netadapt/netadapt.yaml --resolution 512 --netadapt_checkpoint /video-conf/scratch/vibhaa_lam1_new/netadapt/aggregated_results_only_generator_adapted/only_gen/512/${conv_type}_${percent}/generic/00000038-checkpoint.pth.tar;
     done
 done
+
+
+# netadapt for motivation section at 1024
+python measure_times.py --config config/paper_configs/netadapt/netadapt_1024_only_gen.yaml --resolution 1024 --netadapt_checkpoint /video-conf/scratch/vibhaa_lam1_new/netadapt/aggregated_results_only_generator_adapted/only_gen/1024/normal_10/xiran/00000059-checkpoint.pth.tar
